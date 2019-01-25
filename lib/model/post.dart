1ca0ca86e428401d0a4a6bb15fed2d2b32fc6542
IBAS <1091021117@qq.com>
2019-01-25 14:24:49 +0800

    lesson_7_over

--- lib/model/post.dart
+++ lib/model/post.dart
@@ -12,14 +12,104 @@ class Post {
 
 // 图片是直接复制 百度图片的图片的链接，可能会存在失效的问题
 final List<Post> posts = [
-  Post(
-    title: 'Candy Shop',
-    author: 'Mohamed Chahin',
-    imageUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3735008710,1033072259&fm=27&gp=0.jpg',
-  ),
-  Post(
-    title: 'Big data',
-    author: 'Mohamed Chahin',
-    imageUrl: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2088206210,2085020975&fm=27&gp=0.jpg',
-  )
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/hamburg-3846525__340.jpg",
+		title: "title：liggraphy",
+		author: "author：4940"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/ibis-3809147__340.jpg",
+		title: "title：Alexas_Fotos",
+		author: "author：4961"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/forest-3833973__340.jpg",
+		title: "title：darkmoon1968",
+		author: "author：5023"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/man-3915438__340.jpg",
+		title: "title：pixel2013",
+		author: "author：2559"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/road-3856796__340.jpg",
+		title: "title：jplenio",
+		author: "author：2725"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/harley-davidson-3794909__340.jpg",
+		title: "title：Tama66",
+		author: "author：4191"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/crystal-ball-photography-3894871__340.jpg",
+		title: "title：Alexas_Fotos",
+		author: "author：4345"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/rays-3902368__340.jpg",
+		title: "title：marcelkessler",
+		author: "author：623"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/buzzard-2287699__340.jpg",
+		title: "title：Gentle07",
+		author: "author：6565"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/mushroom-3587888__340.jpg",
+		title: "title：StefleiFotografie",
+		author: "author：122"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/rum-3898745__340.jpg",
+		title: "title：Alexas_Fotos",
+		author: "author：5719"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/flower-3876195__340.jpg",
+		title: "title：MabelAmber",
+		author: "author：791"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/open-fire-3879031__340.jpg",
+		title: "title：Tama66",
+		author: "author：9504"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/kiss-3541905__340.jpg",
+		title: "title：suju",
+		author: "author：4288"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/lichterkette-3834926__340.jpg",
+		title: "title：Couleur",
+		author: "author：6354"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/autumn-3846345__340.jpg",
+		title: "title：9883074",
+		author: "author：5969"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/leaf-3865014__340.jpg",
+		title: "title：MabelAmber",
+		author: "author：1956"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/tree-3822149__340.jpg",
+		title: "title：jplenio",
+		author: "author：1470"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/forest-3877365__340.jpg",
+		title: "title：darkmoon1968",
+		author: "author：2017"
+	),
+	Post(
+		imageUrl: "http://androidpic.sunibas.cn/landscape-3779159__340.jpg",
+		title: "title：Kanenori",
+		author: "author：3710"
+	)
 ];
